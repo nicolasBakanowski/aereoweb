277c20ebd658fc5fc72f58e381055aa3e876a30f
Nicolas Bakanowski <nicolasbakanowski@gmail.com>
2020-06-11 20:11:36 -0300

    apply ajax

---
+++ javascript/miprogramacion.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("#btncompra").click(function(){
+        $("#centro").load("compraExitosa.html");
+    });
+    $("#faqs").click(function(){
+        $("#centro").load("paginaFaqs.html");
+    });
+});
