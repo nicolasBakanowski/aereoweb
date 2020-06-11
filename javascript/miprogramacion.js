e0dc47691323448553053c23e010df1ea94219e0
Nicolas Bakanowski <nicolasbakanowski@gmail.com>
2020-06-11 20:42:19 -0300

    repair inicio

--- javascript/miprogramacion.js
+++ javascript/miprogramacion.js
@@ -5,4 +5,11 @@ $(document).ready(function() {
     $("#faqs").click(function(){
         $("#centro").load("paginaFaqs.html");
     });
+    $("#tyc").click(function(){
+        $("#centro").load("paginaTermCond.html");
+    });
+    $("#inicio").click(function(){
+        $("#centro").load("inicio.html");
+    });
+    $('#centro').load('inicio.html');
 });
